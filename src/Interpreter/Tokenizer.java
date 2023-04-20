e7e907235c267e63662718797fbd89fe37a4fffc
BGOPC <bb8933400@gmail.com>
2023-04-20 20:32:09 +0330

    Tokens, Basic Structure

---
+++ src/Interpreter/Tokenizer.java
@@ -0,0 +1,4 @@
+package src.Interpreter;
+
+public class Tokenizer {
+}
---
+++ src/shell.java
@@ -0,0 +1,9 @@
+package src;
+import java.util.Scanner;
+public class shell {
+    public static Scanner prompt = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String command = prompt.nextLine();
+    }
+}
