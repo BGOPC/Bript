8779c54ba00b77540a141db934e0072bfae9e77f
BGOPC <bb8933400@gmail.com>
2023-04-23 12:49:23 +0330

    Basic Tokenizer

--- src/Interpreter/Tokenizer.java
+++ src/Interpreter/Tokenizer.java
@@ -1,4 +1,14 @@
 package src.Interpreter;
 
+import java.util.ArrayList;
+
 public class Tokenizer {
+    public String commands;
+    public Tokenizer(String commands) {
+        this.commands = commands;
+    }
+    public ArrayList<Tokens> Tokenize(){
+        ArrayList<Tokens> tokens = new ArrayList<Tokens>();
+        return tokens;
+    }
 }
--- src/shell.java
+++ src/shell.java
@@ -1,9 +1,11 @@
 package src;
 import java.util.Scanner;
+import src.Interpreter.*;
 public class shell {
     public static Scanner prompt = new Scanner(System.in);
 
     public static void main(String[] args) {
         String command = prompt.nextLine();
+        Tokenizer commandTokenizer = new Tokenizer(command);
     }
 }
