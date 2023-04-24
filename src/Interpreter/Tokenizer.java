c227cd7d3877fc06ee826b784cecc44f5fd7fae4
BGOPC <bb8933400@gmail.com>
2023-04-24 19:11:52 +0330

    output format

--- src/Interpreter/Tokenizer.java
+++ src/Interpreter/Tokenizer.java
@@ -5,25 +5,24 @@ import java.util.ArrayList;
 public class Tokenizer {
     public String commands;
     public int charCount;
-    public String currentCharacter;
     public Tokenizer(String commands) {
         this.commands = commands;
         this.charCount = 0;
-        this.currentCharacter = "";
     }
     public Token.tokenTypes advance(String value) {
         Token.tokenTypes type = Token.getTokenType(value);
         this.charCount++;
-        this.currentCharacter = String.valueOf(commands.charAt(charCount));
         return type;
     }
     public ArrayList<Token> Tokenize(){
         ArrayList<Token> tokens = new ArrayList<Token>();
         for (char token : this.commands.toCharArray()) {
-            String tokenString = String.valueOf(token);
-            Token.tokenTypes type = this.advance(tokenString);
-            Token currentToken = new Token(type, tokenString);
-            tokens.add(currentToken);
+            if (!(String.valueOf(token).equals(" ") || String.valueOf(token).equals("\n"))) {
+                String tokenString = String.valueOf(token);
+                Token.tokenTypes type = this.advance(tokenString);
+                Token currentToken = new Token(type, tokenString);
+                tokens.add(currentToken);
+            }
         }
         return tokens;
     }
--- src/shell.java
+++ src/shell.java
@@ -6,11 +6,14 @@ public class shell {
     public static Scanner prompt = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.print("Command !#> ");
         String command = prompt.nextLine();
         Tokenizer commandTokenizer = new Tokenizer(command);
         ArrayList<Token> Tokenized = commandTokenizer.Tokenize();
+        String output = "[ ";
         for (Token token : Tokenized) {
-            System.out.println(token.toString());
+            output += token.toString() + ", ";
         }
+        System.out.println(output+"]");
     }
 }
