7394e98c0d0b693ce0fb47b71bdc439ae8d08d6a
BGOPC <bb8933400@gmail.com>
2023-04-28 18:13:34 +0330

    Changed The PrintNestedList to yieldnestedlist
    Instead Of printing One by one it returns an Stream<Object> and the use of it will be declared in the main void

--- src/shell.java
+++ src/shell.java
@@ -1,20 +1,23 @@
 package src;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.stream.Stream;
+
 import src.Tokenize.*;
 import src.parse.Parser;
 
 public class shell {
     public static Scanner prompt = new Scanner(System.in);
-    public static void printNestedList(ArrayList<Object> list) {
-        for (Object item : list) {
+    public static Stream<Object> yieldNestedList(ArrayList<Object> list) {
+        return list.stream().flatMap(item -> {
             if (item instanceof String) {
-                System.out.println(item);
+                return Stream.of(item);
             } else if (item instanceof ArrayList) {
-                ArrayList<Object> ItemObject = (ArrayList<Object>) item;
-                printNestedList(ItemObject);
+                return yieldNestedList((ArrayList<Object>) item);
+            } else {
+                return Stream.empty();
             }
-        }
+        });
     }
 
     public static void main(String[] args) throws Exception {
@@ -27,7 +30,8 @@ public class shell {
             ArrayList<Token> Tokenized = commandTokenizer.Tokenize();
             Parser TokenizedParser = new Parser(Tokenized);
             ArrayList<Object> ParsedTokens = TokenizedParser.ParseTokens();
-
+            Stream<Object> ParsedTokensStream = yieldNestedList(ParsedTokens);
+            ParsedTokensStream.forEach(System.out::println);
         }
     }
 }
